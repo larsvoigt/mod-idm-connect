cad594345ac27f9a7afa84572649ced7b80f1c5d
alb3rtino <alb3rtino@users.noreply.github.com>
2021-11-11 10:37:13 +0100

    Add /searchidm endpoint

--- src/main/java/org/folio/rest/impl/Constants.java
+++ src/main/java/org/folio/rest/impl/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
 
   public static final String TABLE_NAME_CONTRACTS = "contract";
   public static final String BASE_PATH_CONTRACTS = "/idm-connect/contract"; // NOSONAR
+  public static final String MSG_IDM_URL_NOT_SET = "IDM_URL environment variable not set.";
 
   private Constants() {}
 }
